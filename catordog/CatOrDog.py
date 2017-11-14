d5804179107a5fd7cbeffb49ad68d89668c8da79
robin.shie <robin.shie@outlook.com>
2017-11-14 19:16:22 +0800

    fully training

--- catordog/CatOrDog.py
+++ catordog/CatOrDog.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[12]:
+# In[1]:
 
 
 import numpy as np
@@ -10,7 +10,7 @@ import cv2
 
 np.random.seed(1024)
 
-n = 100
+n = 1000
 X = np.zeros((n, 224, 224, 3), dtype=np.uint8)
 y = np.zeros((n, 1), dtype=np.uint8)
 
@@ -111,103 +111,9 @@ model.compile(optimizer=adam,
 """
 
 
-# In[5]:
-
-
-model.fit(X_train, y_train, batch_size=16, epochs=5, validation_data=(X_test, y_test))
-
-
-# In[6]:
-
-
-print('\nTesting ------------')
-
-loss, accuracy = model.evaluate(X_test, y_test)
-
-print('\ntest loss: ', loss)
-print('\ntest accuracy: ', accuracy)
-
-
-# In[7]:
-
-
-from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
-
-SVG(model_to_dot(model).create(prog='dot', format='svg'))
-
-
 # In[8]:
 
 
-zip([x.name for x in model.layers], range(len(model.layers)))
+model.fit(X_train, y_train, batch_size=64, epochs=5, validation_data=(X_test, y_test))
+model.save('train_result_model.h5')
 
-
-# In[9]:
-
-
-weights = model.layers[177].get_weights()[0]
-model2 = Model(model.input, [model.layers[172].output, model.output])
-import h5py
-model2.save("train_model.h5")
-
-print("saved")
-# In[10]:
-
-"""
-import matplotlib.pyplot as plt
-import random
-
-plt.figure(figsize=(12, 14))
-for i in range(16):
-    plt.subplot(4, 4, i+1)
-    img = cv2.imread('test/test/%d.jpg' % random.randint(1, 12500))
-    img = cv2.resize(img, (224, 224))
-    x = img.copy()
-    x.astype(np.float32)
-    out, prediction = model2.predict(np.expand_dims(x, axis=0))
-
-    prediction = prediction[0]
-    out = out[0]
-
-    if prediction < 0.5:
-        plt.title('cat %.2f%%' % (100 - prediction*100))
-    else:
-        plt.title('dog %.2f%%' % (prediction*100))
-
-    cam = (prediction - 0.5) * np.matmul(out, weights)
-    cam -= cam.min()
-    cam /= cam.max()
-    cam -= 0.2
-    cam /= 0.8
-
-    cam = cv2.resize(cam, (224, 224))
-    heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
-    heatmap[np.where(cam <= 0.2)] = 0
-
-    out = cv2.addWeighted(img, 0.8, heatmap, 0.4, 0)
-
-    plt.axis('off')
-    plt.imshow(out[:,:,::-1])
-
-
-# In[11]:
-
-
-img = cv2.imread('2.jpg')
-img = cv2.resize(img, (224, 224))
-x = img.copy()
-x.astype(np.float32)
-out, prediction = model2.predict(np.expand_dims(x, axis=0))
-
-prediction = prediction[0]
-out = out[0]
-
-if prediction < 0.5:
-    plt.title('cat %.2f%%' % (100 - prediction*100))
-else:
-    plt.title('dog %.2f%%' % (prediction*100))
-plt.imshow(img)
-
-
-"""
